018510c7bf519150c0ffd7bc285fa834d756b25d
Elaine0916 <55180609+Elaine0916@users.noreply.github.com>
2020-11-10 16:05:09 +0800

    設定完成

---
+++ 2DGame_Again/Assets/Scripts/LevelManger.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManger : MonoBehaviour
+{
+    public void NextLevel(string NameLv)
+    {
+        SceneManager.LoadScene(NameLv);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
--- 2DGame_Again/Assets/Scripts/Player.cs
+++ 2DGame_Again/Assets/Scripts/Player.cs
@@ -1,15 +1,29 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 
     public GameObject Final;
 
+    public Text textcount;
+    public int count;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送們惹");
-        Final.SetActive(true);
+        if (collision.name == "傳送門")
+        {
+            Final.SetActive(true);
+        }
+
+        if (collision.tag == "櫻桃")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = "櫻桃數量:" + count;
+        }
+        
     }
 
 }
